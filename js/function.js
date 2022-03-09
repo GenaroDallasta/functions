36515d76330df4b5951b95f1eda9fa6eda3aab21
Genaro <gedallasta@gmail.com>
2022-03-08 23:38:28 -0300

    correccion

--- js/function.js
+++ js/function.js
@@ -1,15 +1,14 @@
-function checkAge(age) {
-  if (age > 17) {
-    return true;
-  } else {
-    return confirm("¿Tenés permiso de tus padres?");
+let producto = prompt("¿Qué estabas buscando?");
+let cantidad = prompt("¿Qué cantidad buscabas?");
+
+function compra(a, b) {
+  if (a != "hamburguesa") {
+    alert("No tenemos eso, no te gustaria buscar otra cosa?");
+  } else if (a === "hamburguesa" && b == 2) {
+    alert("Solo por hoy 2 x 1! Te quedan a $250 cada una!");
+  } else if (a === "hamburguesa" && b == 1) {
+    alert("Serian $500");
   }
 }
 
-let age = prompt("¿Qué edad tenés?");
-
-if (checkAge(age)) {
-  alert("Acceso otorgado");
-} else {
-  alert("Acceso denegado");
-}
+compra(producto, cantidad);
